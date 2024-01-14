1272d1cd366ee6cd89ffc6e9b1aee326108e5d19
Vikram <singh.vikramsingh06@gmail.com>
2024-01-14 15:42:02 -0500

    4032 lab 1

---
+++ Lab-1/README.md
@@ -0,0 +1,2 @@
+# Doccker Hub image link
+https://hub.docker.com/repository/docker/vikramsingh2580/my-typescript-app/general
---
+++ Lab-1/my-typescript-app/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey this Web App created by - Vikram Singh  
+        </p>
+        <p>
+          Web App with Docker
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
